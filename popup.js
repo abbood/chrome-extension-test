4855068cf901ccd91826db343e61d15ccf18d1c3
Abdullah <abdullah.bakhach@gmail.com>
2017-01-20 08:50:12 +0200

    failed attempt at executing google js

--- popup.js
+++ popup.js
@@ -1,4 +1,7 @@
 (function () {
     var otherWindows = chrome.extension.getBackgroundPage();
     console.log(otherWindows.backgroundFunction());
+    chrome.tabs.executeScript(null, {file:"https://apis.google.com/js/client.js?onload=checkAuth"});
+    document.getElementById('authorize-button').onclick = otherWindows.handleAuthClick(event)
 })();
+
